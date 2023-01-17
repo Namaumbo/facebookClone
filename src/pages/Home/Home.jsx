ca6a72f6e51aaa4cf9c208e1510ea1d8fac67c50
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2023-01-17 13:27:03 +0200

    build

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Feed from '../../components/Feed/Feed'
-import RightBar from '../../components/rightBar/RightBar'
+import RightBar from '../../components/rightBar/Rightbar'
 import SideBar from '../../components/sideBar/SideBar'
 import TopBar from '../../components/topBar/TopBar'
 import './Home.css'
